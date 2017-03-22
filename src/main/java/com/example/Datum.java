6fff140b54eb34c51a080a3413e149edcade4b85
Addison Blanda <tuf18810@temple.edu>
2017-03-22 17:10:34 -0400

    Create Datum.java

---
+++ src/main/java/com/example/Datum.java
@@ -0,0 +1,12 @@
+public class Datum {
+
+    public String raw;
+    public String[] names;
+    public String[] data;
+
+    public Datum(String raw, String[] names, String[] data) {
+        this.raw = raw;
+        this.names = names;
+        this.data = data;
+    }
+}
