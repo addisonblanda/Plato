9734cbd1c7dda25c82f70dc51296bd2461bcc71e
Addison Blanda <tuf18810@temple.edu>
2017-03-22 14:47:56 -0400

    Update DemoApplication.java

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -11,29 +11,137 @@ import java.io.*;
 @SpringBootApplication
 public class DemoApplication {
   
-  public String runpy(String id) {
+  private static String runpy(String id) {
         try {
 	     // run the Unix "ls" command
             // using the Runtime exec method:
-            Process p = Runtime.getRuntime().exec("python hello.py "+id);
+            Process p = Runtime.getRuntime().exec("python information.py");
             
             BufferedReader stdInput = new BufferedReader(new 
                  InputStreamReader(p.getInputStream()));
 
-            BufferedReader stdError = new BufferedReader(new 
-                 InputStreamReader(p.getErrorStream()));
-
             // read the output from the command
-            String s = "";
-	    String str = "";
+            String s;
+	    StringBuilder sb = new StringBuilder();
             while ((s = stdInput.readLine()) != null) {
-                str += s;
+                sb.append(s);
             }     
-            return str;
+            return sb.toString();
         }
         catch (IOException e) {
-            return "";
+            e.printStackTrace();
+	    return "";
         }
+  }
+	
+  private static void datumToFile(Datum datum) {
+        String[] names = datum.names;
+        String[] data = datum.data;
+        
+        if(names.length != data.length) {
+            System.err.print("Name must match length!");
+        }
+
+        int len = names.length;
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+
+        try{
+            for(int i = 0; i < len; i++) {
+                if(!data[i].equals("")) {
+                    File f = new File(names[i]);
+                    f.getParentFile().mkdirs();
+                    fw = new FileWriter(f);
+                    bw = new BufferedWriter(fw);
+                    bw.write(data[i]);
+                    bw.close();
+                    fw.close();
+                }
+            }
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+
+                if (bw != null)
+                    bw.close();
+
+                if (fw != null)
+                    fw.close();
+
+            } catch (IOException ex) {
+
+                ex.printStackTrace();
+
+            }
+
+        }
+    }
+
+  private static Datum fileToDatum(String raw) {
+        File[] directories = new File("data/").listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return file.isDirectory();
+            }
+        });
+
+        assert directories != null;
+
+        List<String> names = new ArrayList<String>();
+        List<String> data = new ArrayList<String>();
+
+        for (File directory : directories) {
+            File[] files = new File(directory.toString()).listFiles();
+            assert files != null;
+            for (File f : files) {
+                try {
+                    String s = readFile(f.toString(), StandardCharsets.UTF_8);
+                    names.add(f.toString());
+                    data.add(s);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return new Datum(raw, names.toArray(new String[0]), data.toArray(new String[0]));
+  }
+
+  private static String readFile(String path, Charset encoding)
+            throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+  }
+
+  private static void rawToFile(String raw) {
+        try {
+            PrintWriter writer = new PrintWriter("input.txt", "UTF-8");
+            writer.println(raw);
+            writer.close();
+        } catch (IOException e) {
+            // do nothing
+        }
+  }
+	
+  private static boolean deleteDirectory(File directory) {
+        if(directory.exists()){
+            File[] files = directory.listFiles();
+            if(null!=files){
+                for(int i=0; i<files.length; i++) {
+                    if(files[i].isDirectory()) {
+                        deleteDirectory(files[i]);
+                    }
+                    else {
+                        files[i].delete();
+                    }
+                }
+            }
+        }
+        return(directory.delete());
   }
 
   @RequestMapping("/")
@@ -47,10 +155,14 @@ public class DemoApplication {
   String hello() {
     return "Hello from Heroku!";
   }
-  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-  public @ResponseBody String get(@PathVariable("id") String id) {
-    String str = "Hello there " + id + "!";
-    return str + runpy(id);
+  @RequestMapping(value = "/{datum}", method = RequestMethod.GET)
+  public @ResponseBody Datum get(@PathVariable("datum") Datum datum) {
+        datumToFile(datum);
+        rawToFile(raw);
+        runpy();
+        Datum response = fileToDatum(raw);
+	deleteDirectory(new File("data"));
+	return response;
   }
 
   public static void main(String[] args) {
