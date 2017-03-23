865915cde9e2a38b28ce09fb66a866f248473f07
Addison Blanda <tuf18810@temple.edu>
2017-03-22 22:51:44 -0400

    Update Datum.java

--- src/main/java/com/example/Datum.java
+++ src/main/java/com/example/Datum.java
@@ -1,17 +1,5 @@
-package com.example;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class Datum {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
     public String raw;
     public String[] names;
     public String[] data;
