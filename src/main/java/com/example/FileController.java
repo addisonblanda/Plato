40d93524dfacd21b70c68768ed2bcfd018587485
Addison Blanda <tuf18810@temple.edu>
2017-03-23 02:39:25 -0400

    Create FileController.java

---
+++ src/main/java/com/example/FileController.java
@@ -0,0 +1,125 @@
+package com.example;
+
+public class FileController {
+
+    private static String csvToFile(String csv) {
+        List<String> arrayList = Arrays.asList(csv.split(","));
+
+        rawToFile(arrayList.get(0));
+
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+        String name, data;
+        int index;
+
+        String cwd = System.getProperty("user.dir");
+
+        try{
+            for(int i = 1; i < arrayList.size(); i++) {
+                index = arrayList.get(i).indexOf('~');
+                if(index != arrayList.get(i).length()-1) {
+                    name = arrayList.get(i).substring(0, index);
+                    data = arrayList.get(i).substring(index+1, arrayList.get(i).length()-1);
+                    System.out.println(name);
+                    File f = new File(cwd + name);
+                    f.getParentFile().mkdirs();
+                    fw = new FileWriter(f);
+                    bw = new BufferedWriter(fw);
+                    bw.write(data);
+                    bw.close();
+                    fw.close();
+                }
+            }
+            return arrayList.get(0);
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+
+                if (bw != null)
+                    bw.close();
+
+                if (fw != null)
+                    fw.close();
+
+            } catch (IOException ex) {
+
+                ex.printStackTrace();
+
+            }
+
+        }
+        return "";
+    }
+
+    private static String fileToCsv(String raw) {
+        File[] directories = new File("data/").listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return file.isDirectory();
+            }
+        });
+        assert directories != null;
+
+        List<String> names = new ArrayList<String>();
+        List<String> data = new ArrayList<String>();
+
+        for (File directory : directories) {
+            File[] files = new File(directory.toString()).listFiles();
+            assert files != null;
+            for (File f : files) {
+                try {
+                    String s = readFile(f.toString(), StandardCharsets.UTF_8);
+                    names.add(f.toString());
+                    data.add(s);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(raw + ",");
+
+        for(int i = 0; i < names.size(); i++) {
+            sb.append(names.get(i) + "~" + data.get(i) + ",");
+        }
+        return sb.toString().substring(0, sb.toString().length()-1);
+    }
+
+  private static String readFile(String path, Charset encoding)
+            throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+  }
+
+  private static void rawToFile(String raw) {
+        try {
+            PrintWriter writer = new PrintWriter("input.txt", "UTF-8");
+            writer.println(raw);
+            writer.close();
+        } catch (IOException e) {
+            // do nothing
+        }
+  }
+	
+  private static boolean deleteDirectory(File directory) {
+        if(directory.exists()){
+            File[] files = directory.listFiles();
+            if(null!=files){
+                for(int i=0; i<files.length; i++) {
+                    if(files[i].isDirectory()) {
+                        deleteDirectory(files[i]);
+                    }
+                    else {
+                        files[i].delete();
+                    }
+                }
+            }
+        }
+        return(directory.delete());
+  }
+}
